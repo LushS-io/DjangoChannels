ccf946b01b9eb13eee4d122881dc524b4055fa35
Troy Kirin <troy.kirinhakone@wsu.edu>
2019-06-11 17:46:33 -0700

    tut: part 1 complete

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'channels',
+    'chat',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'chat'
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# mysite/settings.py
+# Channels
+ASGI_APPLICATION = 'mysite.routing.application'
